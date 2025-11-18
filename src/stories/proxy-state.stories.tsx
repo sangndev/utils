011f112e29f1c03d76768d14e0b9f24d6a9b7c0d
sangnd <sang.nguyen@navigosgroup.com>
2025-11-18 18:11:04 +0700

    feat: proxy-state

--- src/stories/proxy-state.stories.tsx
+++ src/stories/proxy-state.stories.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import type { Meta } from "@storybook/react-vite";
 import { subscribe, proxy } from "../../src/helpers/proxy-state";
 
@@ -8,11 +9,18 @@ const meta = {
 export default meta;
 
 export function Basic() {
-  const a = proxy({ count: 0, foo: { bar: "bazz" } });
+  const a = useRef(proxy({ count: 0, foo: { bar: "bazz" } }));
   // subscribe(a, "count", () => {});
   // a.count++;
-  subscribe(a.foo, 'bar', () => {})
-  a.foo.bar = 'rezzz'
+  subscribe(a.current, "foo", (record) => {
+    console.log("foo changed: ", record);
+  });
+  // unsubscribe();
+  const unsubscribe = subscribe(a.current.foo, "bar", (record) => {
+    console.log("bar changed: ", record);
+  });
+  a.current.foo.bar = "yayayayayaoayaoyoayoay";
+  // unsubscribe()
 
   // console.log(a.count)
   return <div>basic ne</div>;
